37a3fca9d632800b099d32de18bf60d4bcb19159
lucaaas-araujo <lucasaraujofernandes.dev@hotmail.com>
2026-05-06 16:17:55 -0300

    feat: corrige div

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,6 @@
 function Home() {
   return (
     <>
-      <div className="flex justify-center bg-amber-300">
-        <h1 className="bg-red-600 font-bold text-3xl">Teste</h1>
-      </div>
       <div
         style={{
           backgroundColor: "#312e81",
