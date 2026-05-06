301f77f12f457658f74bdbbdbccab0ff7ae42f8d
lucaaas-araujo <lucasaraujofernandes.dev@hotmail.com>
2026-05-06 15:46:03 -0300

    Corrigindo estrutura do projeto

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <div className="flex justify-center bg-amber-300">
+        <h1 className="bg-red-600 font-bold text-3xl">Teste</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
