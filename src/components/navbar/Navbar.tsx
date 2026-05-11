d996ec2286d902ae115f09c2d3ae71661dd95788
lucaaas-araujo <lucasaraujofernandes.dev@hotmail.com>
2026-05-11 10:28:13 -0300

    cria crud temas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -24,8 +24,8 @@ function Navbar() {
                     <p>Bem vindo, {usuario.nome}</p>
                     <div className='flex gap-4'>
                         Postagens
-                        Temas
-                        Cadastrar tema
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                         Perfil
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
